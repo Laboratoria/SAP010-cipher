6edf0bf5cc49c1fcd64d63b871093fb9079ff804
Larissa de Paula <larissadepaulagoncalves@gmail.com>
2023-04-24 20:26:11 -0300

    index.html

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,49 @@
 const cipher = {
-  // ...
-};
+  encode: function( deslocamento, texto) {
+    if  ( typeof deslocamento !== 'number' || typeof texto !== 'string' ) {
+      throw new TypeError('Os argumentos devem ser uma string e um número');
+    } else {
+
+      let codificado = "";
+      for (let i = 0; i < texto.length; i++) {
+        let caracter = texto[i];
+        if (caracter.match(/[A-Z]/i)) {
+          const codigo = texto.charCodeAt(i);
+          if (codigo >= 65 && codigo <= 90) {
+            caracter = String.fromCharCode(((codigo - 65 + deslocamento) % 26) + 65);
+          } 
+        }
+        codificado += caracter;
+      }
+      return codificado;
+    }
+  },
+
+  decode: function(deslocamento, texto ) {
+    if (typeof deslocamento !== 'number' || typeof texto !== 'string' ) {
+      throw new TypeError('Os argumentos devem ser uma string e um número');
+    } else{
+
+      let decodificado = "";
+      for (let i = 0; i < texto.length; i++) {
+        let caracter = texto[i];
+        if (caracter.match(/[A-Z]/i)) {
+          const codigo = texto.charCodeAt(i);
+          if (codigo >= 65 && codigo <= 90) {
+            let resultado = codigo - 65 - deslocamento;
+            while (resultado < 0) {
+              resultado += 26;
+            }
+            caracter = String.fromCharCode((resultado % 26) + 65)
+          } 
+        }
+        decodificado += caracter;
+      }
+    
+      return decodificado;
+    }
+  }
+
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
+function codificar(){
+  const textoInput = document.querySelector("#textoInput").value.toUpperCase();
+  const deslocamento = parseInt(document.querySelector("#deslocamento").value);
+  const textoCodificado = cipher.encode(deslocamento, textoInput);
+  document.querySelector("#textoOutput").value = textoCodificado;
+}
 
-console.log(cipher);
+function decodificar(){
+  const textoInput = document.querySelector("#textoInput").value.toUpperCase();
+  const deslocamento = parseInt(document.querySelector("#deslocamento").value);
+  const textoDecodificado = cipher.decode( deslocamento, textoInput);
+  document.querySelector("#textoOutput").value = textoDecodificado; 
+}
+
+document.querySelector("#codificarBtn").addEventListener("click", codificar);
+document.querySelector("#decodificarBtn").addEventListener("click", decodificar);
+//console.log(cipher);
